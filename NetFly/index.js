1fcde498e05d39ad42d1cba6cdb8a60c1cc745e0
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-05-22 14:07:49 +0530

    Js Practice

---
+++ NetFly/index.js
@@ -0,0 +1,49 @@
+// console.log(Math.random());
+// if(Math.random()<0.1){
+//     function Sum(a,b) {
+//         return a-b;
+//     }
+//     function mult(a,b) {
+//         return a+b;
+//     }
+//     function Subs(a,b) {
+//         return a/b;
+//     }
+//     function divide(a,b) {
+//         return a**b;
+//     }
+// }
+// else{
+//     function Sum(a,b) {
+//         return a+b;
+//     }
+//     function mult(a,b) {
+//         return a*b;
+//     }
+//     function Subs(a,b) {
+//         return a-b;
+//     }
+//     function divide(a,b) {
+//         return a/b;
+//     }
+// }
+// let c=Sum(11,12);
+// let d=mult(11,12);
+// let e=Subs(11,12);
+// let f=divide(11,12);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f)
+a=['Crazy','Amazing','Fire']
+console.log(a.length)
+let b=Math.floor(Math.random()*(a.length-1))
+console.log(b)
+console.log(a[b])
+c=['Engine','Foods','Garments']
+console.log(c[b])
+d=['Bros','Limited','Hub']
+console.log(d[b])
+console.log(a[b]+" "+c[b]+" "+d[b])
+let f=2
+console.log(Math.f)
