2ace1745c07d7d305f86d54916ee8954adbe0a25
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-05-24 20:47:06 +0530

    login page

---
+++ Spotify/loginscript.js
@@ -0,0 +1,21 @@
+console.log("Login Page reached")
+let googlebutton=document.getElementById("GoogleLogin");
+googlebutton.addEventListener("click",function(){
+    alert("Google Login Started")
+})
+let facebookbutton=document.getElementById("FacebookLogin");
+facebookbutton.addEventListener("click",function(){
+    alert("Facebook Login Started")
+})
+let applebutton=document.getElementById("AppleLogin");
+applebutton.addEventListener("click",function(){
+    alert("Apple Login Started")
+})
+let phonebutton=document.getElementById("PhoneLogin");
+phonebutton.addEventListener("click",function(){
+    alert("Phone Login Started")
+})
+let githubbutton=document.getElementById("GithubLogin");
+githubbutton.addEventListener("click",function(){
+    alert("Github Login Started")
+})
