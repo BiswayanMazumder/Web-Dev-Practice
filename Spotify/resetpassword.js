0ccda2bf5339460c5421db46d92c00a72368eb78
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-05-25 11:42:05 +0530

    Removed

--- Spotify/resetpassword.js
+++ Spotify/resetpassword.js
@@ -1,12 +1,17 @@
 console.log("Reset Password Page reached")
 let reset = document.getElementById("ResetPassword");
 let isadded = false;
-
+let emailsent=false;
 reset.addEventListener("click", function () {
     let emailAddress = document.getElementById("emailaddress");
 
     if (emailAddress.value.includes("@")) {
         console.log("Email found");
+        console.log(emailAddress.value);
+        if (!isadded) {
+            document.querySelector(".textfield").innerHTML += "<div class='error' style='color: green;font-size: 15px;font-weight: 700;position: relative;text-align: start;justify-content: start;'>Reset Email Sent Successfully</div>";
+            emailsent = true;
+        }
         emailAddress.style.border = "1px solid gray";
     } else {
         console.log("Email not found");
