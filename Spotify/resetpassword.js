22c754d8eaa716aa96d5814759f12ec56bfdfed7
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-05-25 11:03:50 +0530

    Js corrected

--- Spotify/resetpassword.js
+++ Spotify/resetpassword.js
@@ -1,15 +1,21 @@
 console.log("Reset Password Page reached")
-let EmailAddress = document.getElementById("emailaddress");
 let reset = document.getElementById("ResetPassword");
-let isadded=false;
+let isadded = false;
+
 reset.addEventListener("click", function () {
-    // alert("Email Sent")
-    if (EmailAddress.value.includes("@")) {
-        console.log("Email found")
-        EmailAddress.style.border = "1px solid gray"
+    let emailAddress = document.getElementById("emailaddress");
+
+    if (emailAddress.value.includes("@")) {
+        console.log("Email found");
+        emailAddress.style.border = "1px solid gray";
     } else {
-        console.log("Email not found")
-        EmailAddress.style.border = "2px solid red"    
+        console.log("Email not found");
+        emailAddress.style.border = "2px solid red";
+        if (!isadded) {
+            document.querySelector(".textfield").innerHTML += "<div class='error' style='color: red;font-size: 15px;font-weight: 500;position: relative;text-align: start;justify-content: start;'>This field is required</div>";
+            isadded = true;
+        }
     }
-})
+});
+
 
