3782b575a628604893f80ec0f57f36a662c1250c
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-05-28 19:53:14 +0530

    Logged out user will be logged out and logged in will be logge in forever till logged out

--- Spotify/loggedinpagescript.js
+++ Spotify/loggedinpagescript.js
@@ -1,5 +1,41 @@
 // console.log('Welcome to logged in page')
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail, onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyAy0-j5ZJJl6VST50_Y2JV_0MJKqhc3-7w",
+    authDomain: "grovito-admin.firebaseapp.com",
+    projectId: "grovito-admin",
+    storageBucket: "grovito-admin.appspot.com",
+    messagingSenderId: "914981071784",
+    appId: "1:914981071784:web:6312a727ac7602b2c78b9d",
+    measurementId: "G-G4DXEDMJT8"
+};
 
+// Initialize Firebase
+var functioncalled=false;
+function getuser(){
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const uid = user.uid;
+        //   isloggedin=true;
+          console.log('signed in')
+        //   window.location.replace("loggedinpage.html")
+          functioncalled = true;
+          // ...
+        } else {
+          // User is signed out
+          // ...
+          window.location.replace("index.html")
+          console.log('signed out')
+        }
+      });
+    }
+getuser();
 const letters = '0123456789ABCDEF';
 let color = '#';
 let profilepic = document.querySelector('.profilepicture')
