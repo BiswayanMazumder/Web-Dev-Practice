fd64b3a0d4bab484509b4f19270f72d7720d480d
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-06-26 16:21:18 +0530

    TV Shows making ready

--- NetFly/tvshow.js
+++ NetFly/tvshow.js
@@ -15,7 +15,7 @@ async function fetchrecommendation(){
         const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/recommendations?language=en-US&page=1`, options);
     const data = await response.json();
     const posterPaths = data.results.slice(0, 10).map(movie => movie.poster_path);
-    console.log(posterPaths);
+    // console.log(posterPaths);
     for(var i=0;i<posterPaths.length;i++){
         relatedsection.innerHTML += `<div class="img${i}">
                 <img src=${'https://image.tmdb.org/t/p/w500'+posterPaths[i]}
@@ -26,3 +26,9 @@ async function fetchrecommendation(){
       }
 }
 await fetchrecommendation();
+var posterholder=document.querySelector('.headingimg')
+var PosterPath='https://image.tmdb.org/t/p/w500'+posterpath
+async function fetchposter(){
+posterholder.innerHTML=`<img src=${PosterPath} alt="" width="100%" height="720px">`
+}
+await fetchposter();
